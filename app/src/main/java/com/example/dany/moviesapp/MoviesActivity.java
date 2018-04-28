1f0b6b96bb9eb1d625711a431f169705ebbf6eb8
Dany9624 <36449174+Dany9624@users.noreply.github.com>
2018-04-28 17:15:16 +0300

    movies activity

--- app/src/main/java/com/example/dany/moviesapp/MoviesActivity.java
+++ app/src/main/java/com/example/dany/moviesapp/MoviesActivity.java
@@ -3,7 +3,7 @@ package com.example.dany.moviesapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MoviesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
